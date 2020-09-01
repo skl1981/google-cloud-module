4074129a59acac5d87ddbcc43bc262afdd6ff241
Siarhei_Hryshchanka <zedex@tut.by>
2020-09-01 18:37:25 +0000

    day3_terraform_gcp

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Installs nginx and enable
-if [[ ! $(systemctl list-unit-files | grep nginx) ]]
-    then sudo yum install nginx -y && sudo systemctl start nginx && sudo systemctl enable nginx
-fi
