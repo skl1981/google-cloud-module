5bcddf1e7be69c6063cd112717c9ced313f70075
Dzmitry Mezhva <dzmitry_mezhva@epam.com>
2020-09-02 14:48:32 +0300

    ver2.5

--- Day2/disk.tf
+++ Day2/disk.tf
@@ -2,7 +2,6 @@ resource "google_compute_disk" "default" {
   name  = "terraform-disk"
   type  = "pd-standard"
   size  = 200
-  zone  = "us-central1-c"
 }
 
 resource "google_compute_attached_disk" "default" {
