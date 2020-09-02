40d250548c38eaadcb055ce3bc57262699b43c59
vvv <vvv@vvv.vvv>
2020-09-02 23:01:58 +0300

    Day 2 first version

--- Day2/main.tf
+++ Day2/main.tf
@@ -3,3 +3,93 @@ provider "google" {
   region  = "us-central1"
 }
 
+#################################################################
+#  Creating VPC network
+
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "${var.student_name}-vpc"
+  description             = "${var.student_name}-vpc-network"
+  auto_create_subnetworks = false
+}
+
+#################################################################
+#  Creating firewalls rules
+
+resource "google_compute_firewall" "external_access" {
+  name    = "${var.student_name}-ext-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
+  }
+}
+
+resource "google_compute_firewall" "internal_access" {
+  name    = "${var.student_name}-int-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.${var.student_id}.1.0/24"]
+}
+
+#################################################################
+# Creating auto_create_subnetworks
+
+resource "google_compute_subnetwork" "public" {
+  name          = "public-subnetwork"
+  ip_cidr_range = "10.12.1.0/24"
+  network       = google_compute_network.vpc_network.id
+  description   = "${var.student_name}-public subnetwork"
+}
+
+resource "google_compute_subnetwork" "private" {
+  name          = "private-subnetwork"
+  ip_cidr_range = "10.12.2.0/24"
+  network       = google_compute_network.vpc_network.id
+  description   = "${var.student_name}-private subnetwork"
+}
+
+##################################################################
+# Creating Vm instance
+
+resource "google_compute_instance" "default" {
+  name                = var.name
+  machine_type        = var.machine_type
+  zone                = var.zone
+  deletion_protection = var.deletion_protection
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.size
+      type  = var.type
+    }
+  }
+
+  network_interface {
+    network    = "${var.student_name}-vpc"
+    subnetwork = google_compute_subnetwork.public.id
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+       sudo yum update
+       sudo yum install -y nginx
+       sudo systemctl enable nginx
+       sudo systemctl start nginx
+       echo "<h1>Hello from ${var.student_name}</h1>" > /usr/share/nginx/html/index.html
+    SCRIPT
+}
--- Day2/output.tf
+++ Day2/output.tf
@@ -1,4 +1,7 @@
-output "ip" {
+output "External ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
 
+output "Network_name" {
+  value = "${google_compute_network.vpc_network.name}"
+}
